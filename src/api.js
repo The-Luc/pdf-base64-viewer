6599bbe493f1a1d711215203a95e5008d00e6a07
luctt <luc.thi@dirox.net>
2022-12-23 17:15:31 +0700

    upgrade parser limit

--- src/api.js
+++ src/api.js
@@ -43,7 +43,7 @@ router.post('/', (req, res) => {
   res.send('');
 });
 
-app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.json({ limit: '999mb' }));
 app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
